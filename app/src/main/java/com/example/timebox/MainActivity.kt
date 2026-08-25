af9970e04a73c2c1bd96606a0dc9a58ae7783dd2
stoic-warrior <132925460+stoic-warrior@users.noreply.github.com>
2026-08-26 03:20:48 +0900

    fix: 시스템 바 영역 침범 수정

--- app/src/main/java/com/example/timebox/MainActivity.kt
+++ app/src/main/java/com/example/timebox/MainActivity.kt
@@ -12,6 +12,9 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -100,6 +103,7 @@ fun TimeboxApp() {
         modifier = Modifier
             .fillMaxSize()
             .background(theme.backgroundBrush())
+            .windowInsetsPadding(WindowInsets.systemBars)
             .padding(10.dp)
     ) {
         DateBar(
